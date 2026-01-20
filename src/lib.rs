b45977e895770943fff8b293d094dcbff144856a
Casey Rodarmor <casey@rodarmor.com>
2026-01-20 12:46:53 -0800

    Use `$CARGO_BIN_EXE_{name}` if avialable

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
-use std::path::PathBuf;
+use std::{env, path::PathBuf};
 
 /// Takes the name of a binary target and returns the path to the target's executable.
 pub fn executable_path(name: &str) -> PathBuf {
-  let mut path = std::env::current_exe().unwrap();
+  if let Some(value) = env::var_os(format!("CARGO_BIN_EXE_{}", name)) {
+    return value.into();
+  }
+
+  let mut path = env::current_exe().unwrap();
   path.pop();
   if path.ends_with("deps") {
     path.pop();
   }
-  let exe = String::from(name) + std::env::consts::EXE_SUFFIX;
+  let exe = String::from(name) + env::consts::EXE_SUFFIX;
   path.push(exe);
   path
 }
